ff506e90fd20762e2c474f6ae4684a8dafa2c959
Steven Green <sg1sg2sg3@hotmail.co.uk>
2017-09-08 15:12:44 +0100

    Updated ddsim script to include ddsim_steer.py

--- Simulation.py
+++ Simulation.py
@@ -2,8 +2,9 @@ from Logic.CondorSupervisorLogic import *
 
 # Universal Arguments
 compactFile = '/cvmfs/ilc.desy.de/sw/x86_64_gcc49_sl6/v01-19-04/lcgeo/v00-13-04/ILD/compact/ILD_l4_v02/ILD_l4_v02.xml'
+steeringFile = '/usera/sg568/LC/Validation/Reconstruction/DD4HEP/v01-19-04/LCPandoraValidation/ILDConfig/StandardConfig/lcgeo_current/ddsim_steer.py'
 outputPath = '/r04/lc/sg568/Validation/Simulation/DD4HEP/v01-19-04/ILD_l4_v02/'
-useParticleGun = False
+useParticleGun = True
 
 # For StdHep Files
 stdHepFormat = 'ZudsENERGY_(.*?).stdhep'
@@ -12,11 +13,14 @@ nEvtsPerStdHep = 1000
 
 # Edit for use of Particle Gun
 particleForGun = 'kaon0L'
-energyOfPartilceForGun = 20
+#energyOfPartilceForGun = 20
 numberOfParticlesFromGun = 10000
 numberOfParticlesPerFileFromGun = 1000
 
-CondorSupervisorLogic(compactFile, outputPath, useParticleGun = useParticleGun, particleForGun = particleForGun, energyOfPartilceForGun = energyOfPartilceForGun, numberOfParticlesFromGun = numberOfParticlesFromGun, numberOfParticlesPerFileFromGun = numberOfParticlesPerFileFromGun, stdHepFormat = stdHepFormat, stdHepPath = stdHepPath, nEvtsPerStdHep = nEvtsPerStdHep)
+energies = [30, 40, 50, 60, 70, 80, 90, 100]
+
+for energyOfPartilceForGun in energies:
+    CondorSupervisorLogic(compactFile, steeringFile, outputPath, useParticleGun = useParticleGun, particleForGun = particleForGun, energyOfPartilceForGun = energyOfPartilceForGun, numberOfParticlesFromGun = numberOfParticlesFromGun, numberOfParticlesPerFileFromGun = numberOfParticlesPerFileFromGun, stdHepFormat = stdHepFormat, stdHepPath = stdHepPath, nEvtsPerStdHep = nEvtsPerStdHep)
 
 # Particle Gun Particles - http://geant4.cern.ch/UserDocumentation/UsersGuides/ForApplicationDeveloper/html/AllResources/Control/UIcommands/_gun_.html
 
